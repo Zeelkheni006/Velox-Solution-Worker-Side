9e749ac0d74ba5808c5f43aa3f8768bf62e4d55f
Krishna codewexy <krishnarudani.codewexy@gmail.com>
2026-04-15 18:26:27 +0530

    remove all files

--- lib/core/App_Safety/app_safety.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-void logPrint(String message) {
-  if (kDebugMode) {
-    print(message);
-  }
-}
--- lib/core/utils/app_responsive.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-double rs(BuildContext context, double value) {
-  double width = MediaQuery.of(context).size.width;
-
-  if (width < 600) return value;
-  if (width < 900) return value * 1.2;
-  return value * 1.4;
-}
